151b82b5c21615eeac5ab21a764819566e0d572b
Nisfu Nisfinia Nur Mustika Sari <nisfunisfi25@gmail.com>
2026-05-31 10:30:11 +0700

    revisi register dan menambahkan login

--- freshfish-api/app/Http/Controllers/API/AuthController.php
+++ freshfish-api/app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -14,7 +15,16 @@ class AuthController extends Controller
         $request->validate([
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6'
-        ]);
+        ],
+        [
+            'email.required' => 'Email wajib diisi',
+            'email.email' => 'Format email tidak valid',
+            'email.unique' => 'Email sudah terdaftar',
+
+            'password.required' => 'Password wajib diisi',
+            'password.min' => 'Password minimal 6 karakter'
+        ]
+        );
 
         // SIMPAN USER
         $user = User::create([
@@ -25,6 +35,36 @@ class AuthController extends Controller
         // RESPONSE JSON
         return response()->json([
             'message' => 'Register berhasil',
+            'user' => $user
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        // VALIDASI
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // CEK USER
+        $user = User::where('email', $request->email)->first();
+
+        // CEK PASSWORD
+        if (!$user || !Hash::check($request->password, $user->password)) {
+
+            return response()->json([
+                'message' => 'Email atau password salah'
+            ], 401);
+        }
+
+        // BUAT TOKEN
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // RESPONSE
+        return response()->json([
+            'message' => 'Login berhasil',
+            'token' => $token,
             'user' => $user
         ]);
     }
